b3b71daf699ccd0f9a26908f5c3fc212945969c7
Rahman123L <135105426+Rahman123L@users.noreply.github.com>
2024-07-03 14:58:08 +0700

    Update design.vb

--- design.vb
+++ design.vb
@@ -1,40 +1,180 @@
-'
-'ListView1
-'
-Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.No, Me.Title, Me.Genre, Me.Tahun, Me.Sutradara})
-Me.ListView1.HideSelection = False
-Me.ListView1.Location = New System.Drawing.Point(27, 100)
-Me.ListView1.Name = "ListView1"
-Me.ListView1.Size = New System.Drawing.Size(750, 236)
-Me.ListView1.TabIndex = 2
-Me.ListView1.UseCompatibleStateImageBehavior = False
-Me.ListView1.View = System.Windows.Forms.View.Details
-'
-'No
-'
-Me.No.Text = "NO"
-Me.No.Width = 50
-'
-'Title
-'
-Me.Title.Text = "TITLE"
-Me.Title.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
-Me.Title.Width = 215
-'
-'Genre
-'
-Me.Genre.Text = "GENRE"
-Me.Genre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
-Me.Genre.Width = 125
-'
-'Tahun
-'
-Me.Tahun.Text = "TAHUN"
-Me.Tahun.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
-Me.Tahun.Width = 117
-'
-'Sutradara
-'
-Me.Sutradara.Text = "SUTRADARA"
-Me.Sutradara.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
-Me.Sutradara.Width = 285
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.No = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Title = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Genre = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Tahun = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Sutradara = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.No, Me.Title, Me.Genre, Me.Tahun, Me.Sutradara})
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(25, 442)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(750, 236)
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'No
+        '
+        Me.No.Text = "NO"
+        Me.No.Width = 50
+        '
+        'Title
+        '
+        Me.Title.Text = "TITLE"
+        Me.Title.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.Title.Width = 215
+        '
+        'Genre
+        '
+        Me.Genre.Text = "GENRE"
+        Me.Genre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.Genre.Width = 125
+        '
+        'Tahun
+        '
+        Me.Tahun.Text = "TAHUN"
+        Me.Tahun.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.Tahun.Width = 117
+        '
+        'Sutradara
+        '
+        Me.Sutradara.Text = "SUTRADARA"
+        Me.Sutradara.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.Sutradara.Width = 285
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.Label1.Location = New System.Drawing.Point(40, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(225, 25)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "REKOMENDASI FILM"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.ListBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(25, 65)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(477, 162)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.Window
+        Me.Button1.Location = New System.Drawing.Point(25, 244)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(146, 40)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Cari Film"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.SystemColors.Window
+        Me.Button2.Location = New System.Drawing.Point(186, 244)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(146, 40)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Data Film"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.Label2.Location = New System.Drawing.Point(40, 398)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(225, 25)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "REKOMENDASI FILM"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.SystemColors.Window
+        Me.Button3.Location = New System.Drawing.Point(25, 698)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(146, 40)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Home"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 816)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Name = "Form1"
+        Me.Text = "FILM"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents No As ColumnHeader
+    Friend WithEvents Title As ColumnHeader
+    Friend WithEvents Genre As ColumnHeader
+    Friend WithEvents Tahun As ColumnHeader
+    Friend WithEvents Sutradara As ColumnHeader
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+End Class
